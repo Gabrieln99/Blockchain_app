d9204e96678b39d1f77d0339635e69243eba5d7d
Gabriel99 <155887339+Gabrieln99@users.noreply.github.com>
2025-10-31 01:08:22 +0100

    Prvi counter

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>My first counter</h1>
+    <h2>{{ counter }}</h2>
+    <button @click="smanji">-</button>
+
+    <button @click="povecaj">+</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+function smanji() {
+  counter.value--
+}
+
+function povecaj() {
+  counter.value++
+}
+</script>
